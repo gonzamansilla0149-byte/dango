2daba75a03945720f2a2e02221f4a9a76576fda4
gonzamansilla0149-byte <gonzamansilla0149@gmail.com>
2026-02-23 12:16:13 -0300

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -54,8 +54,80 @@ function updateCartCount() {
   }
 }
 
+function renderCartPage() {
+
+  const container = document.getElementById("cart-items");
+  const totalEl = document.getElementById("cart-total");
+
+  if (!container) return;
+
+  const cart = getCart();
+
+  if (cart.length === 0) {
+    container.innerHTML = "<p>Tu carrito está vacío</p>";
+    totalEl.textContent = "$0";
+    return;
+  }
+
+  let total = 0;
+
+  container.innerHTML = "";
+
+  cart.forEach(item => {
+
+    total += item.price * item.quantity;
+
+    container.innerHTML += `
+      <div class="cart-item">
+        <img src="${item.image}" />
+        <div class="cart-info">
+          <h3>${item.name}</h3>
+          <p>$${item.price.toLocaleString()}</p>
+        </div>
+        <div class="cart-quantity">
+          <button onclick="decreaseQty(${item.id})">-</button>
+          <span>${item.quantity}</span>
+          <button onclick="increaseQty(${item.id})">+</button>
+          <button onclick="removeFromCart(${item.id})">✕</button>
+        </div>
+      </div>
+    `;
+  });
+
+  totalEl.textContent = "$" + total.toLocaleString();
+}
+
 // ==========================
+function increaseQty(id) {
+  let cart = getCart();
+  const item = cart.find(p => p.id === id);
+  if (item) item.quantity++;
+  saveCart(cart);
+  renderCartPage();
+}
+
+function decreaseQty(id) {
+  let cart = getCart();
+  const item = cart.find(p => p.id === id);
+  if (item && item.quantity > 1) item.quantity--;
+  saveCart(cart);
+  renderCartPage();
+}
+
+function removeFromCart(id) {
+  let cart = getCart().filter(p => p.id !== id);
+  saveCart(cart);
+  renderCartPage();
+}
+
+const checkoutBtn = document.getElementById("btn-checkout");
+
+if (checkoutBtn) {
+  checkoutBtn.addEventListener("click", () => {
+    window.location.href = "checkout.html";
+  });
+}
 // INICIAR
 // ==========================
-
+document.addEventListener("DOMContentLoaded", renderCartPage);
 document.addEventListener("DOMContentLoaded", updateCartCount);
