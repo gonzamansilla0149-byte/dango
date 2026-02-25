c4a826f8cfa0a130678281d0ecba149ef70e5635
gonzamansilla0149-byte <gonzamansilla0149@gmail.com>
2026-02-25 01:43:22 -0300

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -446,25 +446,6 @@ if (savedView) {
 }
 
 // ============================
-// SIDEBAR MOBILE TOGGLE
-// ============================
-
-if (menuToggle && sidebar) {
-
-  menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    overlay.classList.toggle("active");
-  });
-
-  overlay.addEventListener("click", () => {
-    sidebar.classList.remove("open");
-    overlay.classList.remove("active");
-  });
-
-}
-
-
-// ============================
 // INIT
 // ============================
 
